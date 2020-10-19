f5ed2a4cf9a70ed32ef279624455ea2582a0d5ae
i-alkisev <i.alkisev@gmail.com>
2020-10-20 01:33:17 +0300

    исправил замечания, добавил класс глобального логгера

--- task2/libs/LoggerLib/include/LoggerLib.hpp
+++ task2/libs/LoggerLib/include/LoggerLib.hpp
@@ -3,25 +3,28 @@
 
 #include <string>
 #include <fstream>
+#include <map>
+#include <memory>
 
 namespace log
     {
 
-    enum Level{DEBUG, INFO, WARNING, ERROR};
+    enum class Level{DEBUG, INFO, WARNING, ERROR};
 
     class BaseLogger
     {
         Level level_;
+        void log(const std::string & msg, Level level);
     public:
         virtual ~BaseLogger();
         void debug(const std::string & msg);
         void info(const std::string & msg);
         void warn(const std::string & msg);
         void error(const std::string & msg);
-        void set_level(const Level level);
+        void set_level(Level level);
         Level level() const;
-        virtual void print(const std::string & msg);
-        virtual void flush();
+        virtual void print(const std::string & msg) = 0;
+        virtual void flush() = 0;
     };
 
     struct StdoutLogger: BaseLogger
@@ -35,7 +38,6 @@ namespace log
     {
         void print(const std::string & msg);
         void flush();
-        ~StderrLogger();
     };
 
     struct FileLogger: BaseLogger
@@ -48,6 +50,29 @@ namespace log
         FileLogger(const FileLogger & filelogger);
         std::ofstream file_;
     };
+
+    struct Logger
+    {
+        static Logger & get_instance();
+        const std::unique_ptr<BaseLogger> & get_global_logger() const;
+        void set_global_logger(std::unique_ptr<BaseLogger> global_logger);
+    private:
+        Logger();
+        Logger(const Logger & logger);
+        Logger & operator=(const Logger & logger);
+        std::unique_ptr<BaseLogger> global_logger_;
+    };
+
+    void set_logger_level(Level lvl);
+    void init_with_stderr_logger(Level lvl = Level::DEBUG);
+    void init_with_stdout_logger(Level lvl = Level::DEBUG);
+    void init_with_file_logger(const std::string & path, Level lvl = Level::DEBUG);
+
+    void debug(const std::string & msg);
+    void info(const std::string & msg);
+    void warn(const std::string & msg);
+    void error(const std::string & msg);
+
 }
 
 #endif
--- task2/libs/LoggerLib/src/LoggerLib.cpp
+++ task2/libs/LoggerLib/src/LoggerLib.cpp
@@ -4,34 +4,36 @@
 
 namespace log
 {
+    std::map<Level, std::string> log_heading = {{Level::DEBUG, "DEBUG: "},
+                                                {Level::INFO, "INFO: "},
+                                                {Level::WARNING, "WARNING: "},
+                                                {Level::ERROR, "ERROR: "}};
+
     BaseLogger::~BaseLogger(){}
 
-    void BaseLogger::debug(const std::string & msg){
-        if(level_ == DEBUG){
-            print("DEBUG: " + msg);
+    void BaseLogger::log(const std::string & msg, Level level){
+        if (level >= level_){
+            print(msg);
         }
+    }
+
+    void BaseLogger::debug(const std::string & msg){
+        log(log_heading[Level::DEBUG] + msg, Level::DEBUG);
     }
     
     void BaseLogger::info(const std::string & msg){
-        if(level_ <= INFO){
-            print("INFO: " + msg);
-        }
+        log(log_heading[Level::INFO] + msg, Level::INFO);
     }
 
     void BaseLogger::warn(const std::string & msg){
-        if(level_ <= WARNING){
-            print("WARNING: " + msg);
-        }
+        log(log_heading[Level::WARNING] + msg, Level::WARNING);
     }
 
     void BaseLogger::error(const std::string & msg){
-        print("ERROR: " + msg);
+        log(log_heading[Level::ERROR] + msg, Level::ERROR);
     }
 
     void BaseLogger::set_level(const Level level){
-        if (level > ERROR || level < DEBUG){
-            throw std::runtime_error("invalid level");
-        }
         level_ = level;
     }
 
@@ -61,8 +63,6 @@ namespace log
 
     void StderrLogger::flush(){}
 
-    StderrLogger::~StderrLogger(){}
-
     FileLogger::FileLogger(const std::string & path){
         file_.open(path, std::ios::app);
         if(!file_.is_open()){
@@ -84,4 +84,57 @@ namespace log
     }
 
     FileLogger::FileLogger(const FileLogger & filelogger){};
+
+    Logger & Logger::get_instance(){
+        static Logger logger;
+        return logger;
+    }
+
+    void Logger::set_global_logger(std::unique_ptr<BaseLogger> global_logger){
+        global_logger_ = std::move(global_logger);
+    }
+
+    const std::unique_ptr<BaseLogger> & Logger::get_global_logger() const{
+        return global_logger_;
+    }
+
+    Logger::Logger(){
+        global_logger_ = std::make_unique<StderrLogger>();
+    }
+
+    void init_with_stderr_logger(Level lvl){
+        Logger::get_instance().set_global_logger(std::make_unique<StderrLogger>());
+        set_logger_level(lvl);
+    }
+
+    void init_with_stdout_logger(Level lvl){
+        Logger::get_instance().set_global_logger(std::make_unique<StdoutLogger>());
+        set_logger_level(lvl);
+    }
+
+    void init_with_file_logger(const std::string & path, Level lvl){
+        Logger::get_instance().set_global_logger(std::make_unique<FileLogger>(path));
+        set_logger_level(lvl);
+    }
+
+    void set_logger_level(Level lvl){
+        Logger::get_instance().get_global_logger()->set_level(lvl);
+    }
+
+    void debug(const std::string & msg){
+        Logger::get_instance().get_global_logger()->debug(msg);
+    }
+
+    void info(const std::string & msg){
+        Logger::get_instance().get_global_logger()->info(msg);
+    }
+    
+    void warn(const std::string & msg){
+        Logger::get_instance().get_global_logger()->warn(msg);
+    }
+    
+    void error(const std::string & msg){
+        Logger::get_instance().get_global_logger()->error(msg);
+    }
+    
 }
